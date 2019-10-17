8be4b7e124b52d0da4ce94d3c30186106f717a01
Alberto-Funk <laptopreneur@gmail.com>
2019-10-17 01:18:10 -0700

    fe depl WIP

---
+++ front-end/build/precache-manifest.3f614b1b2381f80e547a7b8cf8c99e1a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1033ed78fe3f53872b550ba2beff4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fe1c39ac7368f410927",
+    "url": "/static/js/2.5fb186d9.chunk.js"
+  },
+  {
+    "revision": "487976a790c979c746b0",
+    "url": "/static/js/main.8abdbd28.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
