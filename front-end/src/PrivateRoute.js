e6029041d88da17c6e37c0f18ba30a4221886f34
Alberto-Funk <laptopreneur@gmail.com>
2019-10-17 00:24:20 -0700

    Auth0 - Auth and CB component - private route component - Dashboard w/ auth setup - nav and routing setup

---
+++ front-end/src/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Redirect, withRouter} from 'react-router-dom'
+import auth0Client from './Auth';
+
+
+function PrivateRoute(props) {
+  return (
+    <div>
+      {auth0Client.isAuthenticated() ?
+        props.children
+        :
+        <Redirect to="/" />
+      }
+    </div>
+  )
+}
+
+export default withRouter(PrivateRoute)
---
+++ front-end/src/pages/profile-dashboard/ProfileDashboard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from "styled-components";
+
+import auth0Client from '../../Auth';
+
+
+function ProfileDashboard() {
+  function getIt() {
+    console.log(auth0Client.getProfile())
+    console.log(auth0Client.getIdToken())
+    console.log(auth0Client.isAuthenticated())
+  }
+  return (
+    <Main>
+      <h1>Helloo Dashboard</h1>
+      <button onClick={getIt}>PROFILE</button>
+    </Main>
+  )
+}
+
+const Main = styled.main`
+  width: 100%;
+  height: 100vh;
+  padding-top: 100px;
+  background-color: pink;
+`;
+
+export default ProfileDashboard;
