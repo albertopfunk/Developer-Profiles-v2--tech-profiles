390fd26d497cc33cf0c3003e7ecedd3e066749f9
Alberto-Funk <laptopreneur@gmail.com>
2019-11-27 04:43:08 -0800

    user education wip

--- front-end/src/pages/profile-dashboard/education/DashboardEducation.js
+++ front-end/src/pages/profile-dashboard/education/DashboardEducation.js
@@ -1,10 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
 
 import { ProfileContext } from "../../../global/context/user-profile/ProfileContext";
 
 function DashboardEducation() {
-  const { loadingUser, user } = useContext(ProfileContext);
+  const { loadingUser, user, addExtra } = useContext(ProfileContext);
+  const [school, setSchool] = useState("")
+  const [fieldOfStudy, setFieldOfStudy] = useState("")
+
+
+
+
+  async function submitAdd(e) {
+    e.preventDefault();
+
+    if (
+      !school &&
+      !fieldOfStudy
+    ) {
+      return;
+    }
+
+    const inputs = {user_id: user.id};
+    if (school) {
+      inputs.school = school;
+      setSchool("");
+    }
+
+    // make dates similar to the credit card exp dates
+    // <MM/YY> to <MM/YY>
+
+    if (fieldOfStudy) {
+      inputs.field_of_study = fieldOfStudy;
+      setFieldOfStudy("");
+    }
+
+    addExtra(inputs, "education");
+  }
 
   console.log("Education", user);
   if (loadingUser) {
@@ -13,6 +45,25 @@ function DashboardEducation() {
   return (
     <Main>
       <h1>Hello Education</h1>
+      <form onSubmit={e => submitAdd(e)}>
+        <input
+          type="text"
+          placeholder="School"
+          value={school}
+          onChange={e => setSchool(e.target.value)}
+        />
+
+        <br />
+
+        <input
+          type="text"
+          placeholder="Field of Study"
+          value={fieldOfStudy}
+          onChange={e => setFieldOfStudy(e.target.value)}
+        />
+
+        <button>Submit</button>
+      </form>
     </Main>
   );
 }
