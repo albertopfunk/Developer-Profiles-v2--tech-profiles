65976c10d8734510b8443abbb60ec196eb2d10f6
Alberto-Funk <laptopreneur@gmail.com>
2020-06-19 12:09:24 -0700

    dash education wip - mostly focused on dates - working on auto-formatting and validation - unable to get selectionRange working - user carot goes to last index if carot range is, - in between chars on input in

--- front-end/src/components/forms/user-extras/EducationForm.js
+++ front-end/src/components/forms/user-extras/EducationForm.js
@@ -2,12 +2,13 @@ import React, { useContext, useState } from "react";
 import { useEffect } from "react";
 
 function EducationForm(props) {
-  const [hasChanged, setHasChanged] = useState(false);
   const [id, SetId] = useState("");
   const [school, setSchool] = useState("");
   const [fieldOfStudy, setFieldOfStudy] = useState("");
   const [dates, setDates] = useState("");
   const [description, setDescription] = useState("");
+
+  let dateRef = React.createRef();
 
   useEffect(() => {
     SetId(props.id);
@@ -17,19 +18,74 @@ function EducationForm(props) {
     setDescription(props.description);
   }, []);
 
-  async function onSchoolChange(value) {
-    if (!hasChanged) {
-      setHasChanged(true);
-    }
-    setSchool(value);
+  function updateEducation(value) {
     props.onEducationChange(props.index, {
       shouldEdit: true,
-      id,
-      school: value,
-      school_dates: dates,
-      field_of_study: fieldOfStudy,
-      education_description: description
+      ...value
     });
+  }
+
+  function onSchoolChange(value) {
+    setSchool(value);
+    updateEducation({ school: value });
+  }
+
+  function onFieldOfStudyChange(value) {
+    setFieldOfStudy(value);
+    updateEducation({ field_of_study: value });
+  }
+
+  function onDatesChange(value) {
+    if (value === "") {
+      setDates("");
+      return;
+    }
+
+    let newValue = "";
+    let carotRange = 0;
+    let keepChecking = true;
+    
+    for (let i = 0; i < value.length; i++) {
+      console.log(value[i], "==", dates[i])
+      if (value[i] !== dates[i] && keepChecking) {
+        console.log("NO MATCH", i)
+        keepChecking = false
+        carotRange = i + 1
+      }
+      if (value[i].match(/^[0-9]$/)) {
+        newValue += value[i];
+      }
+    }
+
+    if (!newValue) {
+      return;
+    }
+
+    if (newValue.length > 0) {
+      if (newValue[0] > 1) {
+        newValue = "0" + newValue;
+      }
+    }
+
+    if (newValue.length > 1) {
+      if (newValue[0] + newValue[1] > 12) {
+        newValue = "0" + newValue;
+      }
+    }
+
+    if (newValue.length > 1) {
+      newValue = `${newValue[0] || ""}${newValue[1] || ""} / ${newValue[2] ||
+        ""}${newValue[3] || ""}`;
+    }
+
+    
+    setDates(newValue, dateRef.current.setSelectionRange(carotRange, carotRange))
+    updateEducation({ school_dates: newValue });
+  }
+
+  function onDescriptionChange(value) {
+    setDescription(value);
+    updateEducation({ education_description: value });
   }
 
   function removeEducation(e) {
@@ -53,10 +109,28 @@ function EducationForm(props) {
         type="text"
         placeholder="Field of Study"
         value={fieldOfStudy}
-        onChange={e => setFieldOfStudy(e.target.value)}
+        onChange={e => onFieldOfStudyChange(e.target.value)}
+      />
+
+      <br />
+
+      <input
+        type="text"
+        ref={dateRef}
+        placeholder="mm/yy"
+        value={dates}
+        onChange={e => onDatesChange(e.target.value)}
+      />
+
+      <br />
+
+      <input
+        type="text"
+        placeholder="description"
+        value={description}
+        onChange={e => onDescriptionChange(e.target.value)}
       />
     </form>
-    // save / cancel buttons?
   );
 }
 
--- front-end/src/pages/profile-dashboard/education/DashboardEducation.js
+++ front-end/src/pages/profile-dashboard/education/DashboardEducation.js
@@ -8,27 +8,20 @@ function DashboardEducation() {
   const { loadingUser, user, addUserExtras } = useContext(ProfileContext);
   const [editInputs, setEditInputs] = useState(false);
   const [education, setEducation] = useState([]);
-  const [addedEducation, setAddedEducation] = useState([]);
-  const [removedEducation, setRemovedEducation] = useState([]);
-
-  // useEffect(() => {
-  // }, [addedEducation, removedEducation]);
-
-  // const memoizedCallback = useCallback(() => {
-  // }, [addedEducation, removedEducation]);
 
   function onEditInputs() {
     setEditInputs(true);
     setEducation(user.education);
   }
 
-  async function onEducationChange(index, state) {
+  function onEducationChange(index, state) {
     let newEduArr = [...education];
-    newEduArr.splice(index, 1, state);
+    let newEduObj = { ...newEduArr[index], ...state };
+    newEduArr.splice(index, 1, newEduObj);
     setEducation(newEduArr);
   }
 
-  async function addEducation() {
+  function addEducation() {
     setEducation([
       ...education,
       {
@@ -39,22 +32,29 @@ function DashboardEducation() {
         education_description: ""
       }
     ]);
-    setAddedEducation([...addedEducation, 1]);
   }
 
-  async function removeEducation(id) {
+  function removeEducation(id) {
     let newEducation = education.filter(edu => edu.id !== id);
     setEducation(newEducation);
-    setRemovedEducation([...removedEducation, 1]);
   }
 
-  async function submitEdit() {
+  function submitEdit() {
     let requests = [];
 
     let checkEdu = {};
     for (let i = 0; i < education.length; i++) {
       if (education[i].id) {
         checkEdu[education[i].id] = true;
+      }
+    }
+
+    for (let i = 0; i < user.education.length; i++) {
+      if (!(user.education[i].id in checkEdu)) {
+        requests.push({
+          method: "DELETE",
+          url: `/extras/education/${user.education[i].id}`
+        });
       }
     }
 
@@ -86,15 +86,6 @@ function DashboardEducation() {
       }
     }
 
-    for (let i = 0; i < user.education.length; i++) {
-      if (!(user.education[i].id in checkEdu)) {
-        requests.push({
-          method: "DELETE",
-          url: `/extras/education/${user.education[i].id}`
-        });
-      }
-    }
-
     if (requests.length === 0) {
       return;
     }
@@ -115,23 +106,30 @@ function DashboardEducation() {
     );
   }
 
+  console.log("===EDU DASH===", education);
+
   return (
     <Main>
       <h1>Hello Education</h1>
       <button onClick={addEducation}>Add New Location</button>
+      <br />
+      <br />
       {education.map((edu, index) => {
         return (
-          <EducationForm
-            key={index}
-            index={index}
-            id={edu.id}
-            school={edu.school}
-            fieldOfStudy={edu.field_of_study}
-            dates={edu.school_dates}
-            description={edu.education_description}
-            onEducationChange={onEducationChange}
-            removeEducation={removeEducation}
-          />
+          <div key={index}>
+            <EducationForm
+              index={index}
+              id={edu.id}
+              school={edu.school}
+              fieldOfStudy={edu.field_of_study}
+              dates={edu.school_dates}
+              description={edu.education_description}
+              onEducationChange={onEducationChange}
+              removeEducation={removeEducation}
+            />
+            <br />
+            <br />
+          </div>
         );
       })}
       <button onClick={submitEdit}>Submit</button>
