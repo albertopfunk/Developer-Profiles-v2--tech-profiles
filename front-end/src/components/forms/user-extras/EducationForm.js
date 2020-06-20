1f6d1d2dd6dcf71a4e1e96a099c8d44a17ce64db
Alberto-Funk <laptopreneur@gmail.com>
2020-06-20 03:57:16 -0700

    dash education wip - seperating useEffect for date w deps - destructured props for useEffect - from-to date format w two inputs

--- front-end/src/components/forms/user-extras/EducationForm.js
+++ front-end/src/components/forms/user-extras/EducationForm.js
@@ -1,27 +1,55 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
+import { dateFormat } from "../../../global/hooks/profile-dashboard";
 
-function EducationForm(props) {
+function EducationForm({
+  userId,
+  userSchool,
+  userFieldOfStudy,
+  userFromDate,
+  userToDate,
+  userDescription,
+  onEducationChange,
+  eduIndex,
+  removeEducation
+}) {
   const [id, SetId] = useState("");
   const [school, setSchool] = useState("");
   const [fieldOfStudy, setFieldOfStudy] = useState("");
-  const [dates, setDates] = useState("");
-  const [carotRange, seCarotRange] = useState(0);
+  const [fromDate, setFromDate] = useState("");
+  const [toDate, setToDate] = useState("");
+  const [fromCarotRange, setFromCarotRange] = useState(0);
+  const [toCarotRange, setToCarotRange] = useState(0);
   const [description, setDescription] = useState("");
 
-  let dateRef = React.createRef();
+  let fromDateRef = React.createRef();
+  let toDateRef = React.createRef();
 
   useEffect(() => {
-    SetId(props.id);
-    setSchool(props.school);
-    setFieldOfStudy(props.fieldOfStudy);
-    setDates(props.dates);
-    setDescription(props.description);
-    dateRef.current.setSelectionRange(carotRange, carotRange);
-  });
+    SetId(userId);
+    setSchool(userSchool);
+    setFieldOfStudy(userFieldOfStudy);
+    setDescription(userDescription);
+  }, [userId, userSchool, userFieldOfStudy, userDescription]);
+
+  useEffect(() => {
+    setFromDate(userFromDate);
+    setToDate(userToDate);
+    fromDateRef.current.setSelectionRange(fromCarotRange, fromCarotRange);
+    toDateRef.current.setSelectionRange(toCarotRange, toCarotRange);
+  }, [
+    userFromDate,
+    userToDate,
+    fromDate,
+    toDate,
+    fromCarotRange,
+    toCarotRange,
+    fromDateRef,
+    toDateRef
+  ]);
 
   function updateEducation(value) {
-    props.onEducationChange(props.index, {
+    onEducationChange(eduIndex, {
       shouldEdit: true,
       ...value
     });
@@ -35,108 +63,67 @@ function EducationForm(props) {
     updateEducation({ field_of_study: value });
   }
 
-  function onDatesChange(value) {
-    if (value === "") {
-      updateEducation({ school_dates: "" });
+  function fromDateChange(value) {
+    const [newValue, newCarotRange] = dateFormat(value, fromDate);
+    if (newValue === false) {
       return;
     }
+    setFromCarotRange(newCarotRange);
+    updateEducation({ school_dates: `${newValue} - ${userToDate}` });
+  }
 
-    let newValue = "";
-    let newCarotRange = 0;
-    let i = 0;
-    if (value.length < dates.length) {
-      i++;
-    }
-    for (i = i; i <= value.length; i++) {
-      if (value[i] !== dates[i]) {
-        newCarotRange = i + 1;
-        break;
-      }
-    }
-
-    for (let i = 0; i < value.length; i++) {
-      if (value[i].match(/^[0-9]$/)) {
-        newValue += value[i];
-      }
-    }
-
-    if (!newValue) {
+  function toDateChange(value) {
+    const [newValue, newCarotRange] = dateFormat(value, toDate);
+    if (newValue === false) {
       return;
     }
-
-    if (newValue.length > 0) {
-      if (newValue[0] > 1) {
-        newValue = "0" + newValue;
-        newCarotRange++;
-      }
-    }
-
-    if (newValue.length > 1) {
-      if (newValue[0] === "0" && newValue[1] === "0") {
-        updateEducation({ school_dates: "0" });
-        return;
-      }
-
-      if (newValue[0] + newValue[1] > 12) {
-        newValue = "0" + newValue;
-        newCarotRange++;
-      }
-    }
-
-    if (newValue.length > 1) {
-      newValue = `${newValue[0] || ""}${newValue[1] || ""} / ${newValue[2] ||
-        ""}${newValue[3] || ""}`;
-    }
-
-    if (newCarotRange === 2) {
-      newCarotRange += 3;
-    }
-
-    seCarotRange(newCarotRange);
-    updateEducation({ school_dates: newValue });
+    setToCarotRange(newCarotRange);
+    updateEducation({ school_dates: `${userFromDate} - ${newValue}` });
   }
 
   function onDescriptionChange(value) {
     updateEducation({ education_description: value });
   }
 
-  function removeEducation(e) {
+  function onRemoveEducation(e) {
     e.preventDefault();
-    props.removeEducation(id);
+    removeEducation(id);
   }
 
-  console.log("===EDU FORM===", school, fieldOfStudy);
+  console.log("===EDU FORM===");
   return (
     <form>
-      <button onClick={e => removeEducation(e)}>Remove</button>
+      <button onClick={e => onRemoveEducation(e)}>Remove</button>
       <input
         type="text"
         placeholder="School"
         value={school}
         onChange={e => onSchoolChange(e.target.value)}
       />
-
       <br />
-
       <input
         type="text"
         placeholder="Field of Study"
         value={fieldOfStudy}
         onChange={e => onFieldOfStudyChange(e.target.value)}
       />
-
       <br />
-
       <input
         type="text"
-        ref={dateRef}
-        placeholder="mm/yy"
-        value={dates}
-        onChange={e => onDatesChange(e.target.value)}
+        ref={fromDateRef}
+        placeholder="MM/YY"
+        value={fromDate}
+        onChange={e => fromDateChange(e.target.value)}
       />
-
+      <span> - </span>
+      <input
+        type="text"
+        ref={toDateRef}
+        placeholder="MM/YY"
+        value={toDate}
+        onChange={e => toDateChange(e.target.value)}
+      />
       <br />
-
       <input
         type="text"
         placeholder="description"
