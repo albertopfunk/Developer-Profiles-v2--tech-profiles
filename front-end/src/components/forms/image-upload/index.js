e514b51fe7e0c98ef6892385c5850efa12f1cb5b
Alberto-Funk <laptopreneur@gmail.com>
2020-08-07 16:04:32 -0700

    image component - component handles removing preview images before submit - and sets current user image to remove - component also displays either prev image or user image

--- front-end/src/components/forms/image-upload/index.js
+++ front-end/src/components/forms/image-upload/index.js
@@ -4,12 +4,13 @@ import { httpClient } from "../../../global/helpers/http-requests";
 import styled from "styled-components";
 
 function ImageUploadForm({ imageInput, setImageInput }) {
-  const { setPreviewImg } = useContext(ProfileContext);
+  const { setPreviewImg, user } = useContext(ProfileContext);
   const [imageLoading, setImageLoading] = useState(false);
   const [imageError, setImageError] = useState(false);
   const [imageSuccess, setImageSuccess] = useState(false);
 
   let imageRef = React.createRef();
+  let imageRemovalRef = React.createRef();
 
   // removing unused preview image on unmount caused bugs
   // current fix is using local storage to remove on mount
@@ -26,7 +27,15 @@ function ImageUploadForm({ imageInput, setImageInput }) {
     };
   }, [setPreviewImg]);
 
-  async function onImageInputChange(e) {
+  function removePreviewImageFromCloudinary() {
+    if (imageInput.id) {
+      httpClient("POST", "/api/delete-image", {
+        id: imageInput.id
+      });
+    }
+  }
+
+  async function uploadImage(e) {
     if (e.target.files.length === 0) {
       return;
     }
@@ -53,56 +62,113 @@ function ImageUploadForm({ imageInput, setImageInput }) {
       return;
     }
 
-    if (imageInput.id) {
-      httpClient("POST", "/api/delete-image", {
-        id: imageInput.id
-      });
-    }
-
+    removePreviewImageFromCloudinary();
     localStorage.setItem("image_id", res.data.id);
     setPreviewImg(res.data);
-    setImageInput(res.data);
+    setImageInput({
+      ...res.data,
+      inputChange: true,
+      shouldRemoveUserImage: false
+    });
     setImageLoading(false);
     setImageError(false);
     setImageSuccess(true);
   }
 
+  function removePreviewImage() {
+    removePreviewImageFromCloudinary();
+    localStorage.removeItem("image_id");
+    setPreviewImg({ image: "", id: "" });
+    setImageInput({
+      image: "",
+      id: "",
+      inputChange: false,
+      shouldRemoveUserImage: false
+    });
+    setImageLoading(false);
+    setImageError(false);
+    setImageSuccess(false);
+  }
+
+  function setImageToRemove() {
+    if (imageRemovalRef.current && imageRemovalRef.current.checked) {
+      setImageInput({ ...imageInput, shouldRemoveUserImage: true });
+    } else {
+      setImageInput({ ...imageInput, shouldRemoveUserImage: false });
+    }
+  }
+
   console.log("=====IMAGEUPLOADFORM + IMG INPUTTTT=====", imageInput);
 
   return (
-    <InputContainer>
-      <label htmlFor="image-upload" id="image-upload-label">
-        Image:
-      </label>
-      <input
-        type="file"
-        id="image-upload"
-        ref={imageRef}
-        name="image-upload"
-        aria-labelledby="image-upload-label"
-        aria-describedby="imageLoading imageError imageSuccess"
-        aria-invalid={imageError}
-        onChange={e => onImageInputChange(e)}
-      />
+    <div>
+      <InputContainer>
+        <label htmlFor="image-upload" id="image-upload-label">
+          Image:
+        </label>
+        <input
+          type="file"
+          id="image-upload"
+          ref={imageRef}
+          name="image-upload"
+          aria-labelledby="image-upload-label"
+          aria-describedby="imageLoading imageError imageSuccess"
+          aria-invalid={imageError}
+          onChange={e => uploadImage(e)}
+        />
 
-      {imageLoading ? (
-        <span id="imageLoading" className="loading-mssg" aria-live="polite">
-          Loading...
-        </span>
-      ) : null}
+        {imageLoading ? (
+          <span id="imageLoading" className="loading-mssg" aria-live="polite">
+            Loading...
+          </span>
+        ) : null}
 
-      {imageError ? (
-        <span id="imageError" className="err-mssg" aria-live="polite">
-          Error uploading image. Please try again
-        </span>
-      ) : null}
+        {imageError ? (
+          <span id="imageError" className="err-mssg" aria-live="polite">
+            Error uploading image. Please try again
+          </span>
+        ) : null}
 
-      {imageSuccess ? (
-        <span id="imageSuccess" className="success-mssg" aria-live="polite">
-          Success!
-        </span>
+        {imageSuccess ? (
+          <span id="imageSuccess" className="success-mssg" aria-live="polite">
+            Success!
+          </span>
+        ) : null}
+      </InputContainer>
+      {user.image || imageInput.image ? (
+        <div style={{ height: "200px", width: "200px" }}>
+          <div
+            style={{
+              position: "absolute",
+              top: "5%",
+              right: "5%",
+              border: "solid",
+              zIndex: "1"
+            }}
+          >
+            {imageInput.image ? (
+              <button onClick={removePreviewImage}>X</button>
+            ) : (
+              <label htmlFor="remove-image">
+                <input
+                  type="checkbox"
+                  id="remove-image"
+                  name="remove-image"
+                  ref={imageRemovalRef}
+                  onChange={setImageToRemove}
+                />
+              </label>
+            )}
+          </div>
+
+          <img
+            style={{ height: "200px", width: "200px" }}
+            src={imageInput.image || user.image}
+            alt="current profile pic"
+          />
+        </div>
       ) : null}
-    </InputContainer>
+    </div>
   );
 }
 
