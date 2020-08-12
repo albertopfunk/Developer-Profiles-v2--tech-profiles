225bf8cadd5fb822f0d40f58b963eb6af3b15072
Alberto-Funk <laptopreneur@gmail.com>
2020-08-12 12:56:52 -0700

    a11y routing announcements - this component now does a few things - it will reset focus on nav change - it will announce nav changes on page load - it will scroll to top on nav change - it also provides the skip links - one downside has to do with SPAs I think - if I reset focus, it will do that, but it will not - reset the a11y tree, so if the user presses - down arrow instead of tab, focus will not be - reset, due to the html document never technically - unmounting. unsure as to how to reset the a11y tree - focus location - some UI and styling still very wip

--- front-end/src/global/helpers/focus-reset/index.js
+++ front-end/src/global/helpers/focus-reset/index.js
@@ -4,24 +4,67 @@ import styled from "styled-components";
 
 function FocusReset({ location, children }) {
   const [previousLocation, setPreviousLocation] = useState("");
+  const [pageLoaded, setPageLoaded] = useState(false);
 
   let focusRef = React.createRef();
 
   useEffect(() => {
+    setTimeout(() => {
+      setPageLoaded(true);
+    }, 300);
+  }, []);
+
+  useEffect(() => {
     if (focusRef.current && previousLocation !== location.pathname) {
       setPreviousLocation(location.pathname);
+      window.scroll(0, 0);
       focusRef.current.focus();
     }
   }, [focusRef, previousLocation, location.pathname]);
 
+  useEffect(() => {
+    if (previousLocation !== location.pathname) {
+      setPageLoaded(false);
+      setTimeout(() => {
+        setPageLoaded(true);
+      }, 300);
+    }
+  }, [previousLocation, location.pathname]);
+
+  const currentLocation = location.pathname.split(/[/-]/).join(" ");
+
   return (
     <FocusContainer tabIndex="-1" ref={focusRef}>
-      <a href={`${location.pathname}#main-heading`} className="skip-link">
-        <span>Skip to Main Content</span>
-      </a>
-      <a href={`${location.pathname}#profile-card`} className="skip-link">
-        <span>Skip to User Card</span>
-      </a>
+      {pageLoaded ? (
+        <p role="alert">{`Navigated to ${currentLocation} • Tech Profiles, press tab for skip links`}</p>
+      ) : null}
+      <ul>
+        <li>
+          <a href={`${location.pathname}#main-heading`} className="skip-link">
+            <span>Skip to Main Content</span>
+          </a>
+        </li>
+
+        {/* <li>
+          <a href={`${location.pathname}#filters-heading`} className="skip-link">
+            <span>Skip to Filters</span>
+          </a>
+        </li> */}
+
+        <li>
+          <a
+            href={`${location.pathname}#page-navigation`}
+            className="skip-link"
+          >
+            <span>Skip to Page Navigation</span>
+          </a>
+        </li>
+        <li>
+          <a href={`${location.pathname}#profile-card`} className="skip-link">
+            <span>Skip to User Card</span>
+          </a>
+        </li>
+      </ul>
       {children}
     </FocusContainer>
   );
