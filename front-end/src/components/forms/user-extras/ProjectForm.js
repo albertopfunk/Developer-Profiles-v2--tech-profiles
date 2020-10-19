b1d23e6c79b7e32d711c5c6628c41bcbb3b102c3
Alberto Preciado <albertopfunk@gmail.com>
2020-10-19 04:50:15 +0000

    dash projects wip - most logic complete, need image upload - very similar to other 2

---
+++ front-end/src/components/forms/user-extras/ProjectForm.js
@@ -0,0 +1,327 @@
+import React, { useContext, useState } from "react";
+import styled from "styled-components";
+
+import { ProfileContext } from "../../../global/context/user-profile/ProfileContext";
+import { validateInput } from "../../../global/helpers/validation";
+import { FORM_STATUS } from "../../../global/helpers/variables";
+
+function ProjectForm({
+  projIndex,
+  userId,
+  userProjectName,
+  userProjectLink,
+  userDescription,
+  updateProject,
+  removeProject,
+}) {
+  const { user } = useContext(ProfileContext);
+
+  const [project, setProject] = useState({
+    project_title: userProjectName,
+    projectChange: false,
+    projectStatus: FORM_STATUS.idle,
+  });
+
+  const [link, setLink] = useState({
+    link: userProjectLink,
+    linkChange: false,
+    linkStatus: FORM_STATUS.idle,
+  });
+
+  const [description, setDescription] = useState({
+    project_description: userDescription,
+    descriptionChange: false,
+    descriptionStatus: FORM_STATUS.idle,
+  });
+
+  function setProjectInput(value) {
+    setProject({
+      ...project,
+      project_title: value,
+      projectChange: true,
+    });
+  }
+
+  function validateProject(e) {
+    let newState;
+    const { value, dataset } = e.target;
+
+    const isUserProject =
+      user.projects.length > 0 && !dataset.inputid.includes("new");
+
+    if (isUserProject && value === user.projects[projIndex].project_title) {
+      newState = {
+        project_title: value,
+        projectChange: false,
+        projectStatus: FORM_STATUS.idle,
+      };
+      setProject(newState);
+      updateProject(projIndex, newState);
+      return;
+    }
+
+    if (value.trim() === "") {
+      newState = {
+        ...project,
+        project_title: "",
+        projectStatus: FORM_STATUS.error,
+      };
+      setProject(newState);
+      updateProject(projIndex, newState);
+      return;
+    }
+
+    if (!project.projectChange) return;
+    if (validateInput("name", value)) {
+      newState = {
+        ...project,
+        projectStatus: FORM_STATUS.success,
+      };
+    } else {
+      newState = {
+        ...project,
+        projectStatus: FORM_STATUS.error,
+      };
+    }
+    setProject(newState);
+    updateProject(projIndex, newState);
+  }
+
+  function setLinkInput(value) {
+    setLink({
+      ...link,
+      link: value,
+      linkChange: true,
+    });
+  }
+
+  function validateLink(e) {
+    let newState;
+    const { value, dataset } = e.target;
+
+    const isUserProject =
+      user.projects.length > 0 && !dataset.inputid.includes("new");
+
+    if (isUserProject && value === user.projects[projIndex].link) {
+      newState = {
+        link: value,
+        linkChange: false,
+        linkStatus: FORM_STATUS.idle,
+      };
+      setLink(newState);
+      updateProject(projIndex, newState);
+      return;
+    }
+
+    if (value.trim() === "") {
+      newState = {
+        ...link,
+        link: "",
+        linkStatus: FORM_STATUS.error,
+      };
+      setLink(newState);
+      updateProject(projIndex, newState);
+      return;
+    }
+
+    if (!link.linkChange) return;
+    if (validateInput("url", value)) {
+      newState = {
+        ...link,
+        linkStatus: FORM_STATUS.success,
+      };
+    } else {
+      newState = {
+        ...link,
+        linkStatus: FORM_STATUS.error,
+      };
+    }
+    setLink(newState);
+    updateProject(projIndex, newState);
+  }
+
+  function setDescriptionInput(value) {
+    setDescription({
+      ...description,
+      project_description: value,
+      descriptionChange: true,
+    });
+  }
+
+  function validateDescription(e) {
+    let newState;
+    const { value, dataset } = e.target;
+
+    const isUserProject =
+      user.projects.length > 0 && !dataset.inputid.includes("new");
+
+    if (isUserProject && value === user.projects[projIndex].project_description) {
+      newState = {
+        project_description: value,
+        descriptionChange: false,
+        descriptionStatus: FORM_STATUS.idle,
+      };
+      setDescription(newState);
+      updateProject(projIndex, newState);
+      return;
+    }
+
+    if (value.trim() === "") {
+      newState = {
+        ...description,
+        project_description: "",
+        descriptionStatus: FORM_STATUS.error,
+      };
+      setDescription(newState);
+      updateProject(projIndex, newState);
+      return;
+    }
+
+    if (!description.descriptionChange) return;
+    if (validateInput("summary", value)) {
+      newState = {
+        ...description,
+        descriptionStatus: FORM_STATUS.success,
+      };
+    } else {
+      newState = {
+        ...description,
+        descriptionStatus: FORM_STATUS.error,
+      };
+    }
+    setDescription(newState);
+    updateProject(projIndex, newState);
+  }
+
+  return (
+    <fieldset>
+      <legend>Project: {project.project_title || "New Project"}</legend>
+
+      <button
+        type="button"
+        aria-label={`Remove ${project.project_title || "New"} Project`}
+        onClick={() => removeProject(userId)}
+      >
+        X
+      </button>
+
+      <InputContainer>
+        <label htmlFor={`project-${userId}`}>Project:</label>
+        <input
+          type="text"
+          autoComplete="organization"
+          id={`project-${userId}`}
+          data-inputid={userId}
+          name="project"
+          className={`input ${
+            project.projectStatus === FORM_STATUS.error ? "input-err" : ""
+          }`}
+          aria-describedby={`project-${userId}-error project-${userId}-success`}
+          aria-invalid={
+            project.project_title.trim() === "" ||
+            project.projectStatus === FORM_STATUS.error
+          }
+          value={project.project_title}
+          onChange={(e) => setProjectInput(e.target.value)}
+          onBlur={(e) => validateProject(e)}
+        />
+        {project.projectStatus === FORM_STATUS.error ? (
+          <span id={`project-${userId}-error`} className="err-mssg">
+            Input is required. Project can only be alphabelical characters, no
+            numbers
+          </span>
+        ) : null}
+        {project.projectStatus === FORM_STATUS.success ? (
+          <span id={`project-${userId}-success`} className="success-mssg">
+            Project is Validated
+          </span>
+        ) : null}
+      </InputContainer>
+
+      <InputContainer>
+        <label htmlFor={`link-${userId}`}>Link:</label>
+        <input
+          type="text"
+          autoComplete="url"
+          id={`link-${userId}`}
+          data-inputid={userId}
+          name="link"
+          className={`input ${
+            link.linkStatus === FORM_STATUS.error ? "input-err" : ""
+          }`}
+          aria-describedby={`link-${userId}-error link-${userId}-success`}
+          aria-invalid={
+            link.link.trim() === "" ||
+            link.linkStatus === FORM_STATUS.error
+          }
+          value={link.link}
+          onChange={(e) => setLinkInput(e.target.value)}
+          onBlur={(e) => validateLink(e)}
+        />
+        {link.linkStatus === FORM_STATUS.error ? (
+          <span id={`link-${userId}-error`} className="err-mssg">
+            Input is required. Link can only be alphabelical characters, no
+            numbers
+          </span>
+        ) : null}
+        {link.linkStatus === FORM_STATUS.success ? (
+          <span id={`link-${userId}-success`} className="success-mssg">
+            Link is Validated
+          </span>
+        ) : null}
+      </InputContainer>
+
+      <InputContainer>
+        <label htmlFor={`description-${userId}`}>Description:</label>
+        <input
+          type="text"
+          id={`description-${userId}`}
+          data-inputid={userId}
+          name="description"
+          className={`input ${
+            description.descriptionStatus === FORM_STATUS.error
+              ? "input-err"
+              : ""
+          }`}
+          aria-describedby={`description-${userId}-error description-${userId}-success`}
+          aria-invalid={
+            description.project_description.trim() === "" ||
+            description.descriptionStatus === FORM_STATUS.error
+          }
+          value={description.project_description}
+          onChange={(e) => setDescriptionInput(e.target.value)}
+          onBlur={(e) => validateDescription(e)}
+        />
+        {description.descriptionStatus === FORM_STATUS.error ? (
+          <span id={`description-${userId}-error`} className="err-mssg">
+            Input is required. description can only be alphabelical characters,
+            no numbers
+          </span>
+        ) : null}
+        {description.descriptionStatus === FORM_STATUS.success ? (
+          <span id={`description-${userId}-success`} className="success-mssg">
+            description is Validated
+          </span>
+        ) : null}
+      </InputContainer>
+    </fieldset>
+  );
+}
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  .input-err {
+    border: solid red;
+  }
+  .err-mssg {
+    color: red;
+    font-size: 0.7rem;
+  }
+  .success-mssg {
+    color: green;
+    font-size: 0.7rem;
+  }
+`;
+
+export default ProjectForm;
--- front-end/src/pages/profile-dashboard/projects/DashboardProjects.js
+++ front-end/src/pages/profile-dashboard/projects/DashboardProjects.js
@@ -1,15 +1,404 @@
-import React, { useContext } from "react";
+/*
+  project_title - same as title
+  project_img - image upload
+  link - url validation
+  project_description - same
+*/
+import React, { useContext, useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import styled from "styled-components";
 
 import { ProfileContext } from "../../../global/context/user-profile/ProfileContext";
+import { FORM_STATUS } from "../../../global/helpers/variables";
+import Announcer from "../../../global/helpers/announcer";
 
+import ProjectForm from "../../../components/forms/user-extras/ProjectForm";
+
+let formSuccessWait;
 function DashboardProjects() {
-  const { user } = useContext(ProfileContext);
+  const { user, addUserExtras } = useContext(ProfileContext);
 
-  console.log("Projects", user);
+  const [formStatus, setFormStatus] = useState(FORM_STATUS.idle);
+  const [projects, setProjects] = useState([]);
+  const [projectsChange, setProjectsChange] = useState(false);
+  const [idTracker, setIdTracker] = useState(1);
+
+  let errorSummaryRef = React.createRef();
+
+  useEffect(() => {
+    if (formStatus === FORM_STATUS.error && errorSummaryRef.current) {
+      errorSummaryRef.current.focus();
+    }
+    // eslint-disable-next-line
+  }, [formStatus]);
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(formSuccessWait);
+    };
+  }, []);
+
+  function setFormInputs() {
+    setFormStatus(FORM_STATUS.active);
+
+    const updatedUserProjects = user.projects.map((proj) => {
+      return {
+        ...proj,
+        projectStatus: FORM_STATUS.idle,
+        projectChange: false,
+
+        linkStatus: FORM_STATUS.idle,
+        linkChange: false,
+
+        descriptionStatus: FORM_STATUS.idle,
+        descriptionChange: false,
+      };
+    });
+
+    setProjects(updatedUserProjects);
+  }
+
+  function updateProject(index, state) {
+    const newProjArr = [...projects];
+    const newProjObj = { ...newProjArr[index], ...state };
+    newProjArr.splice(index, 1, newProjObj);
+    setProjects(newProjArr);
+  }
+
+  function addProject(e) {
+    e.preventDefault();
+
+    setProjects([
+      ...projects,
+
+      {
+        id: `new-${idTracker}`,
+
+        project_title: "",
+        projectStatus: FORM_STATUS.idle,
+        projectChange: false,
+
+        link: "",
+        linkStatus: FORM_STATUS.idle,
+        linkChange: false,
+
+        project_description: "",
+        descriptionStatus: FORM_STATUS.idle,
+        descriptionChange: false,
+      },
+    ]);
+
+    setIdTracker(idTracker + 1);
+    setProjectsChange(true);
+  }
+
+  function removeProject(id) {
+    const filteredProjects = projects.filter((proj) => proj.id !== id);
+    setProjects(filteredProjects);
+    setProjectsChange(true);
+  }
+
+  function checkFormErrors() {
+    return projects.filter((proj) => {
+      let isErrors = false;
+      if (
+        proj.project_title.trim() === "" ||
+        proj.projectStatus === FORM_STATUS.error ||
+        proj.link.trim() === "" ||
+        proj.linkStatus === FORM_STATUS.error ||
+        proj.project_description.trim() === "" ||
+        proj.descriptionStatus === FORM_STATUS.error
+      ) {
+        isErrors = true;
+      }
+
+      return isErrors;
+    });
+  }
+
+  function addNewProjects() {
+    const requests = [];
+
+    projects.forEach((proj) => {
+      if (!Number.isInteger(proj.id)) {
+        requests.push({
+          method: "POST",
+          url: `/extras/new/projects`,
+          data: {
+            project_title: proj.project_title,
+            link: proj.link,
+            project_description: proj.project_description,
+            user_id: user.id,
+          },
+        });
+      }
+    });
+
+    return requests;
+  }
+
+  function removeUserProjects() {
+    const requests = [];
+    const projectsObj = {};
+
+    projects.forEach((proj) => {
+      if (Number.isInteger(proj.id)) {
+        projectsObj[proj.id] = true;
+      }
+    });
+
+    user.projects.forEach((proj) => {
+      if (!(proj.id in projectsObj)) {
+        requests.push({
+          method: "DELETE",
+          url: `/extras/projects/${proj.id}`,
+        });
+      }
+    });
+
+    return requests;
+  }
+
+  function updateUserProjects() {
+    const requests = [];
+
+    projects.forEach((proj) => {
+      if (Number.isInteger(proj.id)) {
+        if (
+          proj.projectChange ||
+          proj.linkChange ||
+          proj.descriptionChange
+        ) {
+          const data = {};
+          if (proj.projectChange) {
+            data.project_title = proj.project_title;
+          }
+
+          if (proj.linkChange) {
+            data.link = proj.link;
+          }
+
+          if (proj.descriptionChange) {
+            data.project_description = proj.project_description;
+          }
+
+          requests.push({
+            method: "PUT",
+            url: `/extras/projects/${proj.id}`,
+            data,
+          });
+        }
+      }
+    });
+
+    return requests;
+  }
+
+  async function submitEdit(e) {
+    e.preventDefault();
+    let requests = [];
+
+    const formErrors = checkFormErrors();
+    if (formErrors.length > 0) {
+      setFormStatus(FORM_STATUS.error);
+      if (errorSummaryRef.current) {
+        errorSummaryRef.current.focus();
+      }
+      return;
+    }
+
+    if (projectsChange) {
+      const newProjRequests = addNewProjects();
+      const removeProjRequests = removeUserProjects();
+      requests = [...newProjRequests, ...removeProjRequests];
+    }
+
+    const userProjChange = projects.filter(
+      (proj) =>
+        Number.isInteger(proj.id) &&
+        (proj.projectChange ||
+          proj.linkChange ||
+          proj.descriptionChange)
+    );
+
+    if (userProjChange.length > 0) {
+      const updatedProjRequests = updateUserProjects();
+      requests = [...requests, ...updatedProjRequests];
+    }
+
+    if (requests.length === 0) {
+      return;
+    }
+
+    setFormStatus(FORM_STATUS.loading);
+    await addUserExtras(requests);
+    formSuccessWait = setTimeout(() => {
+      setFormStatus(FORM_STATUS.idle);
+    }, 1000);
+    setFormStatus(FORM_STATUS.success);
+  }
+
+  if (formStatus === FORM_STATUS.idle) {
+    return (
+      <main id="main-content" tabIndex="-1" aria-labelledby="main-heading">
+        <Helmet>
+          <title>Profile Dashboard Projects • Tech Profiles</title>
+        </Helmet>
+        <h1 id="main-heading">Projects</h1>
+
+        <section aria-labelledby="current-information-heading">
+          <h2 id="current-information-heading">Current Information</h2>
+
+          <button onClick={setFormInputs}>Edit</button>
+
+          {user.projects.length > 0 ? (
+            user.projects.map((proj) => (
+              <div key={proj.id}>
+                <h3>{`Current "${proj.project_title}" Project`}</h3>
+                <ul aria-label={`${proj.project_title} Project`}>
+                  <li>Project Name: {proj.project_title}</li>
+                  <li>Project Link: {proj.link}</li>
+                  <li>Description: {proj.project_description}</li>
+                </ul>
+              </div>
+            ))
+          ) : (
+            <p>No Projects</p>
+          )}
+        </section>
+      </main>
+    );
+  }
+
+  let formErrors;
+  if (formStatus === FORM_STATUS.error) {
+    formErrors = checkFormErrors();
+  }
+
+  console.log("-- Dash Projects --", user);
+
   return (
-    <Main>
-      <h1>Hello Projects</h1>
+    <Main id="main-content" tabIndex="-1" aria-labelledby="main-heading">
+      <Helmet>
+        <title>Profile Dashboard Projects • Tech Profiles</title>
+      </Helmet>
+
+      <h1 id="main-heading">Projects</h1>
+
+      <section aria-labelledby="edit-information-heading">
+        <h2 id="edit-information-heading">Edit Information</h2>
+
+        {formStatus === FORM_STATUS.error ? (
+          <div ref={errorSummaryRef} tabIndex="-1">
+            <h3 id="error-heading">Errors in Submission</h3>
+            <>
+              <strong>
+                Please address the following errors and re-submit the form:
+              </strong>
+
+              {formErrors.length > 0 ? (
+                formErrors.map((proj) => (
+                  <div key={proj.id}>
+                    <h4>{`Current "${
+                      proj.project_title || "New Project"
+                    }" Errors`}</h4>
+
+                    <ul
+                      aria-label={`current ${
+                        proj.project_title || "new project"
+                      } errors`}
+                    >
+                      {proj.project_title.trim() === "" ||
+                      proj.projectStatus === FORM_STATUS.error ? (
+                        <li>
+                          <a href={`#project-${proj.id}`}>project Name Error</a>
+                        </li>
+                      ) : null}
+
+                      {proj.link.trim() === "" ||
+                      proj.linkStatus === FORM_STATUS.error ? (
+                        <li>
+                          <a href={`#link-${proj.id}`}>Link Error</a>
+                        </li>
+                      ) : null}
+
+                      {proj.project_description.trim() === "" ||
+                      proj.descriptionStatus === FORM_STATUS.error ? (
+                        <li>
+                          <a href={`#description-${proj.id}`}>
+                            Description Error
+                          </a>
+                        </li>
+                      ) : null}
+                    </ul>
+                  </div>
+                ))
+              ) : (
+                <>
+                  <p>No Errors, ready to submit</p>
+                  <Announcer
+                    announcement="No Errors, ready to submit"
+                    ariaId="no-errors-announcer"
+                    ariaLive="polite"
+                  />
+                </>
+              )}
+            </>
+          </div>
+        ) : null}
+
+        <div>
+          <button
+            form="projects-form"
+            type="button"
+            aria-label="add new project"
+            onClick={(e) => addProject(e)}
+          >
+            + New Project
+          </button>
+
+          <form id="projects-form" onSubmit={(e) => submitEdit(e)}>
+            {projects.map((proj, index) => {
+              return (
+                <div key={proj.id}>
+                  <ProjectForm
+                    projIndex={index}
+                    userId={proj.id}
+                    userProjectName={proj.project_title}
+                    userProjectLink={proj.link}
+                    userDescription={proj.project_description}
+                    updateProject={updateProject}
+                    removeProject={removeProject}
+                  />
+                </div>
+              );
+            })}
+
+            <button
+              disabled={
+                formStatus === FORM_STATUS.loading ||
+                formStatus === FORM_STATUS.success
+              }
+              type="submit"
+            >
+              {formStatus === FORM_STATUS.active ? "Submit" : null}
+              {formStatus === FORM_STATUS.loading ? "loading..." : null}
+              {formStatus === FORM_STATUS.success ? "Success!" : null}
+              {formStatus === FORM_STATUS.error ? "Re-Submit" : null}
+            </button>
+            <button
+              disabled={
+                formStatus === FORM_STATUS.loading ||
+                formStatus === FORM_STATUS.success
+              }
+              type="reset"
+              onClick={() => setFormStatus(FORM_STATUS.idle)}
+            >
+              Cancel
+            </button>
+          </form>
+        </div>
+      </section>
     </Main>
   );
 }
@@ -22,3 +411,4 @@ const Main = styled.main`
 `;
 
 export default DashboardProjects;
+
