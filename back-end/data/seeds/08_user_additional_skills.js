8bb77169a9af7f523b0742f8d16f330cd53f5fb3
Alberto-Funk <laptopreneur@gmail.com>
2021-04-18 00:17:46 -0700

    db seeds wip - using default data for user_skills

--- back-end/data/seeds/08_user_additional_skills.js
+++ back-end/data/seeds/08_user_additional_skills.js
@@ -1,15 +1,11 @@
+const { seedUserAdditionalSkills } = require("../seedData");
+
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex("user_additional_skills")
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex("user_additional_skills").insert([
-        { user_id: 1, skill_id: 11 }, // React
-        { user_id: 1, skill_id: 8 }, // Front-End
-        { user_id: 1, skill_id: 5 }, // Java
-        { user_id: 1, skill_id: 17 }, // Stripe
-        { user_id: 1, skill_id: 3 }, // Angular
-      ]);
+      return knex("user_additional_skills").insert(seedUserAdditionalSkills);
     });
 };
