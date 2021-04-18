636c9a774c5c130cc3d99699c8bddf213f2e2178
Alberto-Funk <laptopreneur@gmail.com>
2021-04-18 00:18:37 -0700

    db seeds wip - generating 1-5 interested locations for each user

--- back-end/data/seeds/10_user_locations.js
+++ back-end/data/seeds/10_user_locations.js
@@ -3,13 +3,31 @@ exports.seed = function (knex) {
   return knex("user_locations")
     .del()
     .then(function () {
+      let userLocationsArr = [];
+
+      // giving all users 1-5 interested locations
+      for (let i = 1; i <= 50; i++) {
+        let randomAmount = Math.floor(Math.random() * (6 - 1) + 1);
+        let randomUserLocations = [];
+        let randomNumbers = [];
+
+        for (let j = 0; j < randomAmount; j++) {
+          let num = Math.floor(Math.random() * (120 - 1) + 1);
+          let isDup = randomNumbers.includes(num);
+
+          while (isDup) {
+            num = Math.floor(Math.random() * (120 - 1) + 1);
+            isDup = randomNumbers.includes(num);
+          }
+
+          randomNumbers.push(num);
+          randomUserLocations.push({ user_id: i, location_id: num });
+        }
+
+        userLocationsArr = [...userLocationsArr, ...randomUserLocations];
+      }
+
       // Inserts seed entries
-      return knex("user_locations").insert([
-        { user_id: 1, location_id: 1 }, // Los Angeles, CA, USA
-        { user_id: 1, location_id: 6 }, // Tampa, FL, USA
-        { user_id: 1, location_id: 16 }, // New Orleans, LA, USA
-        { user_id: 1, location_id: 4 }, // South Valley, NM, USA
-        { user_id: 1, location_id: 8 }, // Philadelphia, PA, USA
-      ]);
+      return knex("user_locations").insert(userLocationsArr);
     });
 };
