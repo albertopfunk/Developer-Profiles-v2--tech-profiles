38b51fa615ca9f119851d5c98fc2175d83fa249f
Alberto-Funk <laptopreneur@gmail.com>
2021-04-18 00:17:12 -0700

    db seeds wip - user extras for each batch of users

--- back-end/data/seeds/03_experience.js
+++ back-end/data/seeds/03_experience.js
@@ -3,14 +3,21 @@ const faker = require("faker");
 // eslint-disable-next-line
 exports.seed = function (knex, Promise) {
   let experienceArr = [];
-  experienceArr.push({
-    user_id: 1,
-    company_name: faker.company.companyName(),
-    job_title: faker.name.jobTitle(),
-    job_dates: `${faker.date.past()} - Present`,
-    job_description:
-      "Lorem dummy text. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.",
-  });
+
+  for (let i = 1; i <= 50; i++) {
+    let randomNumber = Math.floor(Math.random() * 5);
+    for (let j = 0; j < randomNumber; j++) {
+      let year = 2015 + j;
+      experienceArr.push({
+        user_id: i,
+        company_name: faker.company.companyName(),
+        job_title: faker.name.jobTitle(),
+        job_dates: `${year} - ${year + 1}`,
+        job_description:
+          "Lorem dummy text. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.",
+      });
+    }
+  }
 
   return knex("experience").truncate().insert(experienceArr);
   // Deletes ALL existing entries
--- back-end/data/seeds/04_projects.js
+++ back-end/data/seeds/04_projects.js
@@ -3,15 +3,21 @@ const faker = require("faker");
 // eslint-disable-next-line
 exports.seed = function (knex, Promise) {
   let projectArr = [];
-  projectArr.push({
-    user_id: 1,
-    project_img:
-      "https://res.cloudinary.com/dy5hgr3ht/image/upload/c_scale,h_500/v1590908926/dev-profiles-v1-profile-dashboard/j9d4p6fw6qzb7gn9fn5x.webp",
-    project_title: faker.company.companyName(),
-    link: "https://www.google.com/",
-    project_description:
-      "245char Lorem Ipsum is dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-  });
+
+  for (let i = 1; i <= 50; i++) {
+    let randomNumber = Math.floor(Math.random() * 4);
+    for (let j = 0; j < randomNumber; j++) {
+      projectArr.push({
+        user_id: i,
+        project_img:
+          "https://res.cloudinary.com/dy5hgr3ht/image/upload/c_scale,h_500/v1590908926/dev-profiles-v1-profile-dashboard/j9d4p6fw6qzb7gn9fn5x.webp",
+        project_title: faker.internet.domainName(),
+        link: "albertopfunk.dev",
+        project_description:
+          "245char Lorem Ipsum is dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      });
+    }
+  }
 
   return knex("projects").truncate().insert(projectArr);
   // Deletes ALL existing entries
