229d45254068f98dd1e329e4c69dab1e0499a96b
Alberto Preciado <albertopfunk@gmail.com>
2021-05-09 07:25:34 +0000

    user card layout - summary will not have fallback - grid cell will just collapse - centering

--- front-end/src/components/user-cards/user-card/UserSummary.js
+++ front-end/src/components/user-cards/user-card/UserSummary.js
@@ -1,11 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 
 function UserSummary({ userId, summary }) {
   return (
-    <div id={`profile-${userId}-summary`} className="summary">
-      {summary ? <p>{summary}</p> : <p>No Summary Listed</p>}
-    </div>
+    <SummaryContainer>
+      {summary ? (
+        <p id={`profile-${userId}-summary`} className="summary">
+          {summary}
+        </p>
+      ) : null}
+    </SummaryContainer>
   );
 }
+
+const SummaryContainer = styled.div`
+  grid-column: 1 / 2;
+  grid-row: 3 / 4;
+  place-self: center;
+  text-align: center;
+`;
 
 export default UserSummary;
