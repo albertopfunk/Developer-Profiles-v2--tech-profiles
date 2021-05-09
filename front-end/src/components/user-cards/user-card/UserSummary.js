8544e85c3444b4677ba66b1f124485b45c7d38e6
Alberto Preciado <albertopfunk@gmail.com>
2021-05-09 09:07:05 +0000

    user card layout - summary grid cell still auto

--- front-end/src/components/user-cards/user-card/UserSummary.js
+++ front-end/src/components/user-cards/user-card/UserSummary.js
@@ -18,6 +18,11 @@ const SummaryContainer = styled.div`
   grid-row: 3 / 4;
   place-self: center;
   text-align: center;
+
+  @media (min-width: 1050px) {
+    grid-column: 1 / 3;
+    grid-row: 2 / 3;
+  }
 `;
 
 export default UserSummary;
