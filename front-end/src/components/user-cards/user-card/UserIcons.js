ec390f67bd8bff4edc2fd72f87e2bb5f6d484372
Alberto Preciado <albertopfunk@gmail.com>
2021-05-09 07:35:16 +0000

    user card layout - using SVGs for icons - icons centered and flexed with gap - svgs have viewBox so size determined by container

--- front-end/src/components/user-cards/user-card/UserIcons.js
+++ front-end/src/components/user-cards/user-card/UserIcons.js
@@ -1,14 +1,55 @@
 import React from "react";
+import styled from "styled-components";
+import { ReactComponent as GithubIcon } from "../../../global/assets/profile-github.svg";
+import { ReactComponent as TwitterIcon } from "../../../global/assets/profile-twitter.svg";
+import { ReactComponent as LinkedinIcon } from "../../../global/assets/profile-linkedin.svg";
+import { ReactComponent as WebsiteIcon } from "../../../global/assets/profile-website.svg";
 
 function UserIcons({ github, twitter, linkedin, portfolio }) {
   return (
-    <div className="icons">
-      <p>{github}</p>
-      <p>{twitter}</p>
-      <p>{linkedin}</p>
-      <p>{portfolio}</p>
-    </div>
+    <IconsContainer>
+      {github ? (
+        <a href={github} target="_blank" rel="noreferrer" className="icon">
+          <GithubIcon />
+          <span className="sr-only">github profile(opens in new window)</span>
+        </a>
+      ) : null}
+
+      {twitter ? (
+        <a href={twitter} target="_blank" rel="noreferrer" className="icon">
+          <TwitterIcon />
+          <span className="sr-only">twitter profile(opens in new window)</span>
+        </a>
+      ) : null}
+
+      {linkedin ? (
+        <a href={linkedin} target="_blank" rel="noreferrer" className="icon">
+          <LinkedinIcon />
+          <span className="sr-only">linkedin profile(opens in new window)</span>
+        </a>
+      ) : null}
+
+      {portfolio ? (
+        <a href={portfolio} target="_blank" rel="noreferrer" className="icon">
+          <WebsiteIcon />
+          <span className="sr-only">website profile(opens in new window)</span>
+        </a>
+      ) : null}
+    </IconsContainer>
   );
 }
+
+const IconsContainer = styled.div`
+  grid-column: 1 / 2;
+  grid-row: 4 / 5;
+  place-self: center;
+  display: flex;
+  gap: 15px;
+
+  .icon {
+    width: 30px;
+    height: auto;
+  }
+`;
 
 export default UserIcons;
