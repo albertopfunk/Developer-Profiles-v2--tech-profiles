8b132d3f10858f8be9a023291fb7683b2c2c43cb
Alberto Preciado <albertopfunk@gmail.com>
2021-05-10 08:51:26 +0000

    user card layout - breaking up top/additional into their own sections - making skills into a list - similar layout

--- front-end/src/components/user-cards/user-card/UserSkills.js
+++ front-end/src/components/user-cards/user-card/UserSkills.js
@@ -1,54 +1,64 @@
 import React from "react";
 import styled from "styled-components";
 
-function UserSkills({ topSkills, additionalSkills }) {
+function UserSkills({ topSkills, additionalSkills, userId }) {
   return (
     <SkillsContainer>
-      <div className="top-title">
-        <strong>Top Skills:</strong>
-      </div>
+      <section
+        aria-labelledby={`profile-${userId}-top-skills-header`}
+        className="top-skills"
+      >
+        <h4 id={`profile-${userId}-top-skills-header`} className="title">
+          Top Skills:
+        </h4>
 
-      <div className="top-skills">
-        {topSkills ? (
-          <>
-            {topSkills
-              .split(",")
-              .filter((el, i) => i < 5 && el)
-              .map((skill) => (
-                <p key={skill}>{skill}</p>
-              ))}
+        <ul className="skills">
+          {topSkills ? (
+            <>
+              {topSkills
+                .split(",")
+                .filter((el, i) => i < 5 && el)
+                .map((skill) => (
+                  <li key={skill}>{skill}</li>
+                ))}
 
-            {topSkills.split(",").length > 5 ? (
-              <p>Expand for more skills</p>
-            ) : null}
-          </>
-        ) : (
-          <p>No Top Skills Listed</p>
-        )}
-      </div>
+              {topSkills.split(",").length > 5 ? (
+                <li>Expand for more skills</li>
+              ) : null}
+            </>
+          ) : (
+            <li>No Top Skills Listed</li>
+          )}
+        </ul>
+      </section>
 
-      <div className="additional-title">
-        <strong>Additional Skills:</strong>
-      </div>
+      <section
+        aria-labelledby={`profile-${userId}-additional-skills-header`}
+        className="additional-skills"
+      >
+        <h4 id={`profile-${userId}-additional-skills-header`} className="title">
+          Additional Skills:
+        </h4>
 
-      <div className="additional-skills">
-        {additionalSkills ? (
-          <>
-            {additionalSkills
-              .split(",")
-              .filter((el, i) => i < 5 && el)
-              .map((skill) => (
-                <p key={skill}>{skill}</p>
-              ))}
+        <ul className="skills">
+          {additionalSkills ? (
+            <>
+              {additionalSkills
+                .split(",")
+                .filter((el, i) => i < 5 && el)
+                .map((skill) => (
+                  <li key={skill}>{skill}</li>
+                ))}
 
-            {additionalSkills.split(",").length > 5 ? (
-              <p>Expand for more skills</p>
-            ) : null}
-          </>
-        ) : (
-          <p>No Additional Skills Listed</p>
-        )}
-      </div>
+              {additionalSkills.split(",").length > 5 ? (
+                <li>Expand for more skills</li>
+              ) : null}
+            </>
+          ) : (
+            <li>No Additional Skills Listed</li>
+          )}
+        </ul>
+      </section>
     </SkillsContainer>
   );
 }
@@ -58,7 +68,7 @@ const SkillsContainer = styled.div`
   grid-row: 5 / 6;
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-template-rows: 20% 1fr;
+  grid-template-rows: 1fr;
   grid-gap: 20px;
 
   @media (min-width: 1050px) {
@@ -66,34 +76,58 @@ const SkillsContainer = styled.div`
     grid-row: 3 / 4;
   }
 
-  .top-title {
-    grid-column: 1 / 2;
-    grid-row: 1 / 2;
-    justify-self: start;
+  .skills {
+    list-style: none;
+    padding: 0;
+    margin: 0;
   }
 
   .top-skills {
     grid-column: 1 / 2;
-    grid-row: 2 / 3;
-    place-self: start;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 15px;
-  }
-
-  .additional-title {
-    grid-column: 2 / 3;
     grid-row: 1 / 2;
-    justify-self: start;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 20% 1fr;
+    grid-gap: 20px;
+
+    .title {
+      grid-column: 1 / 2;
+      grid-row: 1 / 2;
+      justify-self: start;
+    }
+
+    .skills {
+      grid-column: 1 / 2;
+      grid-row: 2 / 3;
+      place-self: start;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 15px;
+    }
   }
 
   .additional-skills {
     grid-column: 2 / 3;
-    grid-row: 2 / 3;
-    place-self: start;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 15px;
+    grid-row: 1 / 2;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 20% 1fr;
+    grid-gap: 20px;
+
+    .title {
+      grid-column: 2 / 3;
+      grid-row: 1 / 2;
+      justify-self: start;
+    }
+
+    .skills {
+      grid-column: 2 / 3;
+      grid-row: 2 / 3;
+      place-self: start;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 15px;
+    }
   }
 `;
 
