994089d1dcf71704a50c116d51c7719b32e862cd
Alberto Preciado <albertopfunk@gmail.com>
2021-05-11 08:50:31 +0000

    card extras - setting up section layout for grid - keeping extras on this file since it will only be - markup and styling - setting up projects, edu and exp as desctiption lists

--- front-end/src/components/user-cards/user-extras/UserExtras.js
+++ front-end/src/components/user-cards/user-extras/UserExtras.js
@@ -1,10 +1,7 @@
 import React from "react";
+import styled from "styled-components";
 
-import UserProjects from "./UserProjects";
-import UserEducation from "./UserEducation";
-import UserExperience from "./UserExperience";
-
-function UserExtras({ userExtras, noExtras }) {
+function UserExtras({ userExtras, noExtras, userId }) {
   if (noExtras) {
     return <p>Nothing to Show...</p>;
   }
@@ -19,42 +16,107 @@ function UserExtras({ userExtras, noExtras }) {
   } = userExtras;
 
   return (
-    <section>
-      <section>
-        {topSkills.length > 0 ? (
-          <div className="top-skills">
-            {topSkills.map((skill) => (
-              <p key={skill.name}>{skill.name}</p>
-            ))}
-          </div>
-        ) : null}
+    <ExtrasContainer>
+      {projects.length > 0 ? (
+        <section aria-labelledby={`profile-${userId}-projects-header`}>
+          <h4 id={`profile-${userId}-projects-header`}>Projects:</h4>
 
-        {additionalSkills.length > 0 ? (
-          <div className="additional-skills">
-            {additionalSkills.map((skill) => (
-              <p key={skill.name}>{skill.name}</p>
-            ))}
-          </div>
-        ) : null}
-      </section>
+          <dl>
+            {projects.map((project) => {
+              <div>
+                <dt>{project.project_title}</dt>
+                <dd>{project.project_img}</dd>
+                <dd>{project.project_description}</dd>
+                <dd>{project.link}</dd>
+              </div>;
+            })}
+          </dl>
+        </section>
+      ) : null}
 
-      {projects.length > 0 ? <UserProjects projects={projects} /> : null}
+      {education.length > 0 ? (
+        <section aria-labelledby={`profile-${userId}-education-header`}>
+          <h4 id={`profile-${userId}-education-header`}>Education:</h4>
 
-      {education.length > 0 ? <UserEducation education={education} /> : null}
+          <dl>
+            {education.map((education) => {
+              <div>
+                <dt>{education.school}</dt>
+                <dd>{education.field_of_study}</dd>
+                <dd>{education.school_dates}</dd>
+                <dd>{education.education_description}</dd>
+              </div>;
+            })}
+          </dl>
+        </section>
+      ) : null}
 
       {experience.length > 0 ? (
-        <UserExperience experience={experience} />
+        <section aria-labelledby={`profile-${userId}-experience-header`}>
+          <h4 id={`profile-${userId}-experience-header`}>Experience:</h4>
+
+          <dl>
+            {experience.map((experience) => {
+              <div>
+                <dt>{experience.company_name}</dt>
+                <dd>{experience.job_title}</dd>
+                <dd>{experience.job_dates}</dd>
+                <dd>{experience.job_description}</dd>
+              </div>;
+            })}
+          </dl>
+        </section>
+      ) : null}
+
+      {topSkills.length > 0 ? (
+        <section aria-labelledby={`profile-${userId}-top-skills-header`}>
+          <h4 id={`profile-${userId}-top-skills-header`}>Top Skills:</h4>
+
+          <ul>
+            {topSkills.map((skill) => (
+              <li key={skill.name}>{skill.name}</li>
+            ))}
+          </ul>
+        </section>
+      ) : null}
+
+      {additionalSkills.length > 0 ? (
+        <section aria-labelledby={`profile-${userId}-additional-skills-header`}>
+          <h4 id={`profile-${userId}-additional-skills-header`}>
+            Additional Skills:
+          </h4>
+
+          <ul>
+            {additionalSkills.map((skill) => (
+              <li key={skill.name}>{skill.name}</li>
+            ))}
+          </ul>
+        </section>
       ) : null}
 
       {locations.length > 0 ? (
-        <section className="interested-locations">
-          {locations.map((location) => (
-            <p key={location.name}>{location.name}</p>
-          ))}
+        <section
+          aria-labelledby={`profile-${userId}-interested-locations-header`}
+        >
+          <h4 id={`profile-${userId}-interested-locations-header`}>
+            Interested Locations:
+          </h4>
+
+          <ul>
+            {locations.map((location) => (
+              <li key={location.name}>{location.name}</li>
+            ))}
+          </ul>
         </section>
       ) : null}
-    </section>
+    </ExtrasContainer>
   );
 }
+
+const ExtrasContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(6, auto);
+`;
 
 export default UserExtras;
