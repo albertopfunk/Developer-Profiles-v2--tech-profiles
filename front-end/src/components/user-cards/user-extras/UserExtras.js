b8b9b8c35f38f06ebac1da543b6afcb79868c970
Alberto Preciado <albertopfunk@gmail.com>
2021-05-11 11:02:02 +0000

    card extras - good progress with styling - still wip

--- front-end/src/components/user-cards/user-extras/UserExtras.js
+++ front-end/src/components/user-cards/user-extras/UserExtras.js
@@ -18,96 +18,132 @@ function UserExtras({ userExtras, noExtras, userId }) {
   return (
     <ExtrasContainer>
       {projects.length > 0 ? (
-        <section aria-labelledby={`profile-${userId}-projects-header`}>
-          <h4 id={`profile-${userId}-projects-header`}>Projects:</h4>
+        <ProjectsSection aria-labelledby={`profile-${userId}-projects-header`}>
+          <h4 id={`profile-${userId}-projects-header`} className="title">
+            Projects:
+          </h4>
 
-          <dl>
-            {projects.map((project) => {
-              <div>
+          <dl className="list">
+            {projects.map((project) => (
+              <div key={project.id}>
                 <dt>{project.project_title}</dt>
-                <dd>{project.project_img}</dd>
+                <dt>
+                  <a href={project.link}>{project.link}(opens in new window)</a>
+                </dt>
+                <dd>
+                  <img
+                    style={{ width: "100px" }}
+                    src={project.project_img}
+                    alt=""
+                  />
+                </dd>
                 <dd>{project.project_description}</dd>
-                <dd>{project.link}</dd>
-              </div>;
-            })}
+              </div>
+            ))}
           </dl>
-        </section>
+        </ProjectsSection>
       ) : null}
 
       {education.length > 0 ? (
-        <section aria-labelledby={`profile-${userId}-education-header`}>
-          <h4 id={`profile-${userId}-education-header`}>Education:</h4>
+        <EducationSection
+          aria-labelledby={`profile-${userId}-education-header`}
+        >
+          <h4 id={`profile-${userId}-education-header`} className="title">
+            Education:
+          </h4>
 
-          <dl>
-            {education.map((education) => {
-              <div>
+          <dl className="list">
+            {education.map((education) => (
+              <div key={education.id}>
                 <dt>{education.school}</dt>
+                <dt>{education.school_dates}</dt>
                 <dd>{education.field_of_study}</dd>
-                <dd>{education.school_dates}</dd>
                 <dd>{education.education_description}</dd>
-              </div>;
-            })}
+              </div>
+            ))}
           </dl>
-        </section>
+        </EducationSection>
       ) : null}
 
       {experience.length > 0 ? (
-        <section aria-labelledby={`profile-${userId}-experience-header`}>
-          <h4 id={`profile-${userId}-experience-header`}>Experience:</h4>
+        <ExperienceSection
+          aria-labelledby={`profile-${userId}-experience-header`}
+        >
+          <h4 id={`profile-${userId}-experience-header`} className="title">
+            Experience:
+          </h4>
 
-          <dl>
-            {experience.map((experience) => {
-              <div>
+          <dl className="list">
+            {experience.map((experience) => (
+              <div key={experience.id}>
                 <dt>{experience.company_name}</dt>
+                <dt>{experience.job_dates}</dt>
                 <dd>{experience.job_title}</dd>
-                <dd>{experience.job_dates}</dd>
                 <dd>{experience.job_description}</dd>
-              </div>;
-            })}
+              </div>
+            ))}
           </dl>
-        </section>
+        </ExperienceSection>
       ) : null}
 
       {topSkills.length > 0 ? (
-        <section aria-labelledby={`profile-${userId}-top-skills-header`}>
-          <h4 id={`profile-${userId}-top-skills-header`}>Top Skills:</h4>
+        <TopSkillsSection
+          aria-labelledby={`profile-${userId}-top-skills-header`}
+        >
+          <h4 id={`profile-${userId}-top-skills-header`} className="title">
+            Top Skills:
+          </h4>
 
-          <ul>
+          <ul className="list">
             {topSkills.map((skill) => (
-              <li key={skill.name}>{skill.name}</li>
+              <li className="item" key={skill.name}>
+                {skill.name}
+              </li>
             ))}
           </ul>
-        </section>
+        </TopSkillsSection>
       ) : null}
 
       {additionalSkills.length > 0 ? (
-        <section aria-labelledby={`profile-${userId}-additional-skills-header`}>
-          <h4 id={`profile-${userId}-additional-skills-header`}>
+        <AdditionalSkillsSection
+          aria-labelledby={`profile-${userId}-additional-skills-header`}
+        >
+          <h4
+            id={`profile-${userId}-additional-skills-header`}
+            className="title"
+          >
             Additional Skills:
           </h4>
 
-          <ul>
+          <ul className="list">
             {additionalSkills.map((skill) => (
-              <li key={skill.name}>{skill.name}</li>
+              <li className="item" key={skill.name}>
+                {skill.name}
+              </li>
             ))}
           </ul>
-        </section>
+        </AdditionalSkillsSection>
       ) : null}
 
       {locations.length > 0 ? (
-        <section
+        <InterestedLocationsSection
           aria-labelledby={`profile-${userId}-interested-locations-header`}
         >
-          <h4 id={`profile-${userId}-interested-locations-header`}>
+          <h4
+            id={`profile-${userId}-interested-locations-header`}
+            className="title"
+          >
             Interested Locations:
           </h4>
 
-          <ul>
+          <ul className="list">
             {locations.map((location) => (
-              <li key={location.name}>{location.name}</li>
+              <li className="item" key={location.name}>
+                {location.name}
+              </li>
             ))}
           </ul>
-        </section>
+        </InterestedLocationsSection>
       ) : null}
     </ExtrasContainer>
   );
@@ -117,6 +153,72 @@ const ExtrasContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: repeat(6, auto);
+  grid-gap: 40px;
+
+  section {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 20% 1fr;
+    grid-gap: 10px;
+
+    .title {
+      grid-column: 1 / 2;
+      grid-row: 1 / 2;
+      justify-self: start;
+    }
+
+    .list {
+      grid-column: 1 / 2;
+      grid-row: 2 / 3;
+    }
+
+    ul.list {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+      place-self: start;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 10px;
+
+      .item {
+        border-radius: 10px;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+          rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+        padding: 5px;
+      }
+    }
+  }
+`;
+
+const ProjectsSection = styled.section`
+  grid-column: 1 / 2;
+  grid-row: 1 / 2;
+`;
+
+const EducationSection = styled.section`
+  grid-column: 1 / 2;
+  grid-row: 2 / 3;
+`;
+
+const ExperienceSection = styled.section`
+  grid-column: 1 / 2;
+  grid-row: 3 / 4;
+`;
+
+const TopSkillsSection = styled.section`
+  grid-column: 1 / 2;
+  grid-row: 4 / 5;
+`;
+
+const AdditionalSkillsSection = styled.section`
+  grid-column: 1 / 2;
+  grid-row: 5 / 6;
+`;
+
+const InterestedLocationsSection = styled.section`
+  grid-column: 1 / 2;
+  grid-row: 6 / 7;
 `;
 
 export default UserExtras;
